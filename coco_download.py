0e020746abbe1f32c461a11b4f87c36d211a47e1
dhanrajkatkar <dhanraj.katkar@neosofttech.com>
2020-07-24 12:02:39 +0530

    coco file update

--- coco_download.py
+++ coco_download.py
@@ -1,30 +1,68 @@
 from pycocotools.coco import COCO
-import numpy as np
 import requests
+import os
+from datetime import datetime, timedelta
 
-annFile = "instances_val2017.json"
+# configurations
+required_categories = {8: 'truck'}
+# annFile = "../instances_train2017.json"
+annFile = "../instances_val2017.json"
+folder_img_count = 500
 
 # initialize COCO api for instance annotations
 coco = COCO(annFile)
-
 # display COCO categories and supercategories
 cats = coco.loadCats(coco.getCatIds())
 nms = [cat['name'] for cat in cats]
 print('COCO categories: \n{}\n'.format(' '.join(nms)))
 
 # get all images containing given categories, select one at random
-catIds = coco.getCatIds(catNms=['person'])
+catNms = [category for category in required_categories.values()]
+catIds = coco.getCatIds(catNms=catNms)
 imgIds = coco.getImgIds(catIds=catIds)
-print(len(imgIds))
+total_images = len(imgIds)
+print(total_images)
 images = coco.loadImgs(imgIds)
 
-i = 0
+starting_index = 0
+counter = starting_index
+timer = datetime.now()
 for img in images:
-    print("im: ", img['file_name'])
+    folder_name = 'coco_data/truck/coco_v' + str(counter // folder_img_count)
+    filename = folder_name + '/' + img['file_name']
+
+    if os.path.isfile(filename):
+        counter += 1
+        continue
+
+    print(counter, ": ", img['file_name'])
+
+    if counter % 50 == 0:
+        tt = (datetime.now() - timer).seconds
+        remaining_img = (starting_index + total_images - counter)
+        print('time required for 50 images ', tt, 'remaining time for ', remaining_img, ' images ',
+              remaining_img * timedelta(seconds=tt / 50))
+        timer = datetime.now()
+
     img_data = requests.get(img['coco_url']).content
-    if i > 200:
-        break
-    else:
-        i += 1
-    with open(img['file_name'], 'wb') as handler:
+
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        with open(folder_name + "/annotations_v" + str(counter // folder_img_count) + '.csv', "a+") as myfile:
+            myfile.write('file_name,classes,xmin,ymin,xmax,ymax\n')
+
+    with open(filename, 'wb') as handler:
         handler.write(img_data)
+        annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds)
+        anns = coco.loadAnns(annIds)
+        with open(folder_name + "/annotations_v" + str(counter // folder_img_count) + '.csv', "a+") as myfile:
+            for i in range(len(anns)):
+                xmin = anns[i]["bbox"][0]
+                ymin = anns[i]["bbox"][1]
+                xmax = anns[i]["bbox"][2] + anns[i]["bbox"][0]
+                ymax = anns[i]["bbox"][3] + anns[i]["bbox"][1]
+
+                mystring = img['file_name'] + ',' + required_categories[anns[i]['category_id']] + ',' + str(
+                    xmin) + ',' + str(ymin) + ',' + str(xmax) + "," + str(ymax)
+                myfile.write(mystring + '\n')
+    counter += 1
